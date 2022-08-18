68be4635b943384ff6ff434abe3e75c1d6fc07d5
Vincent Chan <okcdz@diverse.space>
2022-08-18 20:15:34 +0800

    feat: update attributes

--- shared-lib/lib-ot/tests/main.rs
+++ shared-lib/lib-ot/tests/main.rs
@@ -10,7 +10,12 @@ fn main() {
 fn test_documents() {
     let mut document = DocumentTree::new();
     let mut tb = TransactionBuilder::new(&document);
-    tb.insert_nodes(&Position(vec![0]), &vec![NodeData::new("type")]);
+    tb.insert_nodes(&Position(vec![0]), &vec![NodeData::new("text")]);
     let transaction = tb.finalize();
     document.apply(transaction);
+
+    assert!(document.node_at_path(&Position(vec![0])).is_some());
+    let node = document.node_at_path(&Position(vec![0])).unwrap();
+    let node_data = document.arena.get(node).unwrap().get();
+    assert_eq!(node_data.node_type, "text");
 }
